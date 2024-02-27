6faf266e1aefa343b89d957065a6daabb8b53e4f
ByakkoLord <g.fonsecadacosta@gmail.com>
2024-02-26 22:04:32 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+    }),
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const handleCallNotification = async () => {
+    const { status } = await Notifications.getPermissionsAsync();
+
+    if (status !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      
+        return;
+      }
+      console.log('Notification permissions', status);
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'My first local notification',
+          body: 'This is the body of the first local notification',
+          data: { data: 'goes here' },
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    }
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <TouchableOpacity style={styles.button} onPress={handleCallNotification}>
+          <Text>Click Aqui</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +51,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'blue',
+    color: 'black',
+    padding: 10,
+  }
 });
